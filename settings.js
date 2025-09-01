053d9568e5c5a6af45223312511edc5e05b93ed4
hackertadiwa-cloud <hackertadiwa@gmail.com>
2025-09-01 13:44:33 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+
+export default {
+    "prefix": ".",
+    "botName": "KNIGHT-MD",
+    "version": "1.0",
+    "ownerName": "TECHKNIGHT",
+    "ownerNumber": [
+        "263780792734",
+        "208216239681788"
+    ],
+    "owners": [],
+    "autoReact": false,
+    "autoTyping": false,
+    "autoStatusReact": false,
+    "chatbotEnabled": false,
+    "antidelete": true
+};
